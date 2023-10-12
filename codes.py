6fe64b177b8fd7be9130a57deae84ddccbc36952
Dora Ma <1035073267@qq.com>
2023-10-12 14:24:20 +0800

    Update codes.py
    修改窗口提前跳出的问题

--- codes.py
+++ codes.py
@@ -11,48 +11,50 @@ import xlwings as xw
 import os
 
 def combineexcel(path):
-    app = xw.App(visible=True, add_book=False)
-    files = [i  for root, dirs, files in os.walk(path, topdown=False) for i in files]
-    #print(files)
-    new_workbook = app.books.add()
-    for i in range(len(files)):
-        if files[i].endswith(".xls") or files[i].endswith(".xlsx"):
-            wbook02 = app.books.open(path+'\\'+files[i])
-            sheet = wbook02.sheets[0]
-           # for sheet in wbook02.sheets:
-          #  print(wbook02.sheets)
-         #   if len(wbook02.sheets) ==1:
-          #  sheet  =wbook02.sheets[0]
-            if files[i].endswith(".xls"):
-                sheet.name = files[i].replace('.xls','')
-            if files[i].endswith(".xls"):
-                sheet.name = files[i].replace('.xlsx','')
-             #   print(new_workbook.sheets)
-            sheet.copy(before=new_workbook.sheets[0])
-            wbook02.close()
+    if len(path) > 0 :
+        app = xw.App(visible=True, add_book=False)
+        files = [i  for root, dirs, files in os.walk(path, topdown=False) for i in files]
+        #print(files)
+        new_workbook = app.books.add()
+        for i in range(len(files)):
+            if files[i].endswith(".xls") or files[i].endswith(".xlsx"):
+                wbook02 = app.books.open(path+'\\'+files[i])
+                sheet = wbook02.sheets[0]
+               # for sheet in wbook02.sheets:
+              #  print(wbook02.sheets)
+             #   if len(wbook02.sheets) ==1:
+              #  sheet  =wbook02.sheets[0]
+                if files[i].endswith(".xls"):
+                    sheet.name = files[i].replace('.xls','')
+                if files[i].endswith(".xls"):
+                    sheet.name = files[i].replace('.xlsx','')
+                 #   print(new_workbook.sheets)
+                sheet.copy(before=new_workbook.sheets[0])
+                wbook02.close()
 
 
-    new_workbook.save('{}\\{}.xlsx'.format(path,'合并'))  # 保存新工作簿
-    new_workbook.close()  # 关闭新建工作簿
-    app.quit()  # 退出Excel程序
+        new_workbook.save('{}\\{}.xlsx'.format(path,'合并'))  # 保存新工作簿
+        new_workbook.close()  # 关闭新建工作簿
+        app.quit()  # 退出Excel程序
 
 def splitexcel(file_path):
    # file_path =
-    app = xw.App(visible=True, add_book=False)
-    workbook = app.books.open(file_path)
-    worksheet = workbook.sheets
-    for i in worksheet:  # 遍历工作簿中所有工作表
-        new_workbook = app.books.add()  # 新建工作簿
-        new_worksheet = new_workbook.sheets[0]  # 选中新建工作簿中的第1张工作表
-        i.copy(before=new_worksheet)  # 将原来工作簿中的当前工作表复制到新建工作簿的第1张工作表之前if __name__ == '__main__':
+   if len(file_path) > 0 :
+        app = xw.App(visible=True, add_book=False)
+        workbook = app.books.open(file_path)
+        worksheet = workbook.sheets
+        for i in worksheet:  # 遍历工作簿中所有工作表
+            new_workbook = app.books.add()  # 新建工作簿
+            new_worksheet = new_workbook.sheets[0]  # 选中新建工作簿中的第1张工作表
+            i.copy(before=new_worksheet)  # 将原来工作簿中的当前工作表复制到新建工作簿的第1张工作表之前if __name__ == '__main__':
 
-        path = os.path.dirname(file_path)
-        if os.path.exists('{}\拆分后'.format(path)) == False:
-            os.mkdir('{}\拆分后'.format(path))
-   #     print('{}\拆分后\{}.xlsx'.format(path,i.name))
-        new_workbook.save('{}\拆分后\{}.xlsx'.format(path,i.name))  # 保存新工作簿
-        new_workbook.close()  # 关闭新建工作簿
-    app.quit()  # 退出Excel程序
+            path = os.path.dirname(file_path)
+            if os.path.exists('{}\拆分后'.format(path)) == False:
+                os.mkdir('{}\拆分后'.format(path))
+       #     print('{}\拆分后\{}.xlsx'.format(path,i.name))
+            new_workbook.save('{}\拆分后\{}.xlsx'.format(path,i.name))  # 保存新工作簿
+            new_workbook.close()  # 关闭新建工作簿
+        app.quit()  # 退出Excel程序
 def select_folder():
     # 打开文件选择对话框
     folderpath = filedialog.askdirectory()
